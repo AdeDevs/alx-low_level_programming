8008fe6772bc03754e39a422c0dc727d8a06354a
AdeDevs <adeyemiakinyemi01@gmail.com>
2022-03-16 21:10:18 +0100

    C is amazing

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: c is an ascii character
- *
- * Return: 1 (if letter)
+ * _isalpha - checks for alphabet character
+ * @c: character to check
+ * Return: 1 if lower or uppercase, 0 if not alphabet character
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
-	else if (c >= 'A' && c <= 'Z')
+	else
 	{
+		return (0);
+	}
+	_putchar('\n');
+}
